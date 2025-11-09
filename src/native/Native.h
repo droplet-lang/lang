3b0959be472e215ab8f65a6bbf4d7a4f2c01824f
imp-sike <itsnpsulav@gmail.com>
2025-11-09 20:14:29 +0545

    setup a execution point

---
+++ src/native/Native.h
@@ -0,0 +1,23 @@
+/*
+ * ============================================================
+ *  Droplet 
+ * ============================================================
+ *  Copyright (c) 2025 Droplet Contributors
+ *  All rights reserved.
+ *
+ *  Licensed under the MIT License.
+ *  See LICENSE file in the project root for full license.
+ *
+ *  File: Native
+ *  Created: 11/9/2025
+ * ============================================================
+ */
+#ifndef DROPLET_NATIVE_H
+#define DROPLET_NATIVE_H
+
+
+class Native {
+};
+
+
+#endif //DROPLET_NATIVE_H
