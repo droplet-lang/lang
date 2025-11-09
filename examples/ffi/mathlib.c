e926a3fcbaddd2feea76c7e94bacf0b1ad071ad0
imp-sike <itsnpsulav@gmail.com>
2025-11-09 11:59:41 +0545

    add example file

---
+++ examples/ffi/mathlib.c
@@ -0,0 +1,14 @@
+// mathlib.c
+#include <math.h>
+
+int add_numbers(int a, int b) {
+    return a + b;
+}
+
+int square(int x) {
+    return x * x;
+}
+
+double power(double base, double exp) {
+    return pow(base, exp);
+}
