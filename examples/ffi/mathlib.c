24c56cfa10555d00960b163bf0dbd18e3e55cef3
imp-sike <itsnpsulav@gmail.com>
2025-11-11 18:42:03 +0545

    update example

--- examples/ffi/mathlib.c
+++ examples/ffi/mathlib.c
@@ -1,13 +1,5 @@
 #include <stdint.h>
 
-#ifdef _WIN32
-#define API __declspec(dllexport)
-#define CALL_CONV __cdecl
-#else
-#define API
-#define CALL_CONV
-#endif
-
-API int64_t CALL_CONV add_numbers(int64_t a, int64_t b) {
+int64_t add_numbers(int64_t a, int64_t b) {
     return a + b;
 }
